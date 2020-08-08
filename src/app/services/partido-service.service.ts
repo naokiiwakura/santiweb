01b409e68800e8dcd71997fc3c4f1fbdfca54ed6
Elton Naoki Iwakura <elton.naoki@gmail.com>
2020-08-08 12:13:54 -0400

    api de consulta de partido

---
+++ src/app/services/partido-service.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { retry, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PartidoServiceService {
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+
+
+  listarPartidos() {
+    return this.http.get<any[]>(environment.api + '/partido')
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      );
+  }
+
+
+
+  
+
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,4 +10,5 @@
 
 export const environment = {
   production: false,
+  api: 'https://santi-api.herokuapp.com/api',
 };
